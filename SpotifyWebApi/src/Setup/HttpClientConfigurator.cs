deb8ce734dacd452287b02b6a8f8863c4b955187
Tom <tom.git@mailfence.com>
2024-10-28 15:01:47 +0100

    HttpClientConfigurator: null as default token value - for usecases where you want to set it later e.g. via message handler

--- SpotifyWebApi/src/Setup/HttpClientConfigurator.cs
+++ SpotifyWebApi/src/Setup/HttpClientConfigurator.cs
@@ -2,7 +2,7 @@ namespace SpotifyWebApi.Setup;
 
 public static class HttpClientConfigurator
 {
-    public static void Configure(HttpClient client, string? accessToken)
+    public static void Configure(HttpClient client, string? accessToken = null)
     {
         client.BaseAddress = new Uri("https://api.spotify.com/v1/");
         client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", accessToken);
